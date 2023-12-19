3751deb263c00c4f577aa7f4cd7aee1b8487ffa8
Pablo Arango <pabloearangom@gmail.com>
2023-12-19 00:08:58 -0500

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
+import React, { useState } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './Feedback/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (type) => {
+    setFeedback((prevFeedback) => ({ ...prevFeedback, [type]: prevFeedback[type] + 1 }));
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedback;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const { good } = feedback;
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Expresso Café Feedback">
+        <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+      </Section>
+
+      {totalFeedback > 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      ) : (
+        <Section title="Statistics">
+          <Notification message="There is no feedback" />
+        </Section>
+      )}
     </div>
   );
 };
+
+export default App;
